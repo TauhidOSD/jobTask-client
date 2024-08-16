db0f2523b83ec9b119125e2d4e10e55537f7bfa2
Tauhid hossen <tauhidhossen04@gmail.com>
2024-08-17 03:12:43 +0600

    pagination functionalaty added on client side

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,19 +1,47 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-
+import './Products.css'
 const Products = () => {
-   const [products, setProducts] = useState([]);
-   const totalCount = useLoaderData();
-   console.log(totalCount);
-   
- 
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const { count } = useLoaderData();
+  console.log(count);
+  const numberOfPage = Math.ceil(count / itemsPerPage);
+  //  const pages =[]
+  //  for (let i =0; i < numberOfPage; i++){
+  //   pages.push(i)
+  //  }
+  const pages = [...Array(numberOfPage).keys()];
+  console.log(pages);
 
- useEffect(() => {
-    fetch("http://localhost:5000/products")
+  useEffect(() => {
+    fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
       .then((res) => res.json())
       .then((data) => setProducts(data));
-  }, []);
+  }, [currentPage,itemsPerPage]);
+
+  const handleItemsPerPage = e => {
+        const val =parseInt(e.target.value);
+        console.log(val);
+        setItemsPerPage(val);
+        setCurrentPage(0);
+        
+  }
+
+  const handlePrevPage = () =>{
+    if(currentPage > 0){
+      setCurrentPage(currentPage -1);
+    }
+  }
+
+  const handleNextPage = () =>{
+    if(currentPage < pages.length -1){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
 
   return (
     <div className=" grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-4 sm:grid-cols-1 sm:gap-4 space-y-4">
@@ -41,6 +69,27 @@ const Products = () => {
           </div>
         </div>
       ))}
+
+      <div className="pagination text-rose-500 text-center mb-40">
+       <p className="font-semibold">Current page: {currentPage}</p>
+        <button onClick={handlePrevPage}>Prev</button>
+        {
+          pages.map(page => <button className= "  px-2 gap-4 border-2 mr-2 bg-black text-white" 
+            
+            
+            onClick={() => setCurrentPage(page)}
+            key={page}
+             >{page}</button>)
+        }
+        <button onClick={handleNextPage}>Next</button>
+        <select className="bg-green-500" value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+        </select>
+      </div>
     </div>
   );
 };
