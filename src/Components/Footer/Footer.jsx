cc6182aeeb37f7a43a3e6c45766e476afc1c86aa
Tauhid hossen <tauhidhossen04@gmail.com>
2024-08-17 14:59:02 +0600

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
+
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div>
-      <h2>This is footer</h2>
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img className="w-20 rounded-md" src="https://i.postimg.cc/rsh3yvXf/image.png" alt="" />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text even since the 1500s, when an unknown printer took a gally of type and scrambled it to make a type specimen book. </p>
+            {/* <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div> */}
+        </div>
+
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-222-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com -All Right Reserved.
+      </p>
+
     </div>
   );
 };
